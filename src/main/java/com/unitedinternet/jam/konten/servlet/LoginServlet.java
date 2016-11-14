7eceb7825f6aafc4a2e250b0a09d6a8d4a76490e
Andre Grunow <andre.grunow@1und1.de>
2016-11-14 18:14:03 +0100

    Servlet umgebaut

--- src/main/java/com/unitedinternet/jam/konten/servlet/LoginServlet.java
+++ src/main/java/com/unitedinternet/jam/konten/servlet/LoginServlet.java
@@ -5,6 +5,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 /**
  * Created by agrunow on 10.10.16.
@@ -13,6 +14,17 @@ public class LoginServlet extends HttpServlet {
     protected void service(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
-        System.out.println("Juhu, Servlet wurde gestartet.");
+//        String content = backend.getContentForRequest();
+//
+//        if (content ...)
+//            new UI("success.jsp")
+
+//        response.setContentType("text/html");
+//        PrintWriter out = response.getWriter();
+//        out.println("<HTML>");
+//        out.println("<BODY>");
+//        out.println("Juhu, <i>ich</i> komme vom <b>Server</b>.");
+//        out.println("</BODY>");
+//        out.println("</HTML>");
     }
 }
